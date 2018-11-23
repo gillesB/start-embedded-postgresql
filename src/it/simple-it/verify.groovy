20c065a01845249565c5687ff01179a68f8b3e95
Gilles Baatz <gilles.baatz@nc-systems.de>
2018-11-23 14:27:58 +0100

    Integration test creates JOOQ classes

--- src/it/simple-it/verify.groovy
+++ src/it/simple-it/verify.groovy
@@ -1,3 +1,7 @@
 File touchFile = new File( basedir, "target/touch.txt" );
 
 assert touchFile.isFile()
+
+File generatedAccountFile = new File( basedir, "target/generated-sources/jooq/lu/gbaatz/xml_generator/db_model/tables/Account.java");
+
+assert touchFile.isFile()
